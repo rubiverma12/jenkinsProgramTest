a50df76bdb6f34278a852f5a7b304c40a86a7c14
rubi <rubi.verma12@gmail.com>
2024-04-01 16:58:29 +0100

    added message

--- src/main/java/com/example/basedomains/BaseDomainsApplication.java
+++ src/main/java/com/example/basedomains/BaseDomainsApplication.java
@@ -3,6 +3,7 @@ package com.example.basedomains;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//testing for jenkins
 @SpringBootApplication
 public class BaseDomainsApplication {
 
